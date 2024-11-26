62b9b0ad35e99789537107156b6d13ea160aaf23
BIBISARA13 <hajirasara08@gmail.com>
2024-11-26 16:40:19 +0530

    Create .c

---
+++ .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){ 
+   int n,{1=0,12=1, nextterm;
+printf("enter the number of terms:");
+scanf("%d",&n);
+printf(fibonacci series %d,%d",t1, t2);
+for (int i=3;i<=n;i++){
+next term =t1+t2
+printf(",%d", next term):
+t1=t2
+t2 = next term
+}
+
+return 0;
+}
